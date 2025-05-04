886ba340c454572249f1ef80d870bfc546ebb5bc
Gus524 <e.gus.gg@gmail.com>
2025-05-04 12:57:09 -0600

    Update distintas funcionalidades

--- project/config/Global.php
+++ project/config/Global.php
@@ -8,6 +8,6 @@ set_include_path(
     PATH_SEPARATOR . realpath(__DIR__ . '/..') . '/DAO'
 );
 
-define("SITE_URL", "http://192.168.100.2/Project-Web/");
+define("SITE_URL", "http://192.168.100.21/Project-Web/");
 define("ROOT_PATH", "/home/www/Project-Web/");
 define("DEFAULT_URL", "Inicio");
--- project/index.php
+++ project/index.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 
-ini_set("display_errors", 1);
-
 include_once 'config/Global.php';
 include_once 'utilidades/utilidades.php';
 
