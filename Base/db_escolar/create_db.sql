208ac29b6d81f7eb3d117c28e20b590a39d8139b
Gus524 <e.gus.gg@gmail.com>
2025-06-26 23:49:50 -0600

    Changes for Azure

--- Base/db_escolar/create_db.sql
+++ Base/db_escolar/create_db.sql
@@ -10,57 +10,57 @@ CREATE TABLE Institucion (
 );
 
 CREATE TABLE Carrera (
-    abr_carr			CHAR(1) PRIMARY KEY,
-    desc_carr			VARCHAR(64) NOT NULL,
-    no_sem				INT,
+    abr_carr    CHAR(1) PRIMARY KEY,
+    desc_carr    VARCHAR(64) NOT NULL,
+    no_sem	     INT,
     -- max_semestres		INT,
-    id_inst				INT,
+    id_inst	        	INT,
     FOREIGN KEY (id_inst) REFERENCES Institucion(id_inst)
 );
 
 CREATE TABLE Plan_Estudios (
-    id_plan 			INT AUTO_INCREMENT PRIMARY KEY,
-    desc_plan	 		VARCHAR(64) NOT NULL,
-    no_plan				NUMERIC(3), -- 20, 21, etc
-    abr_carr			CHAR(1) NOT NULL,
-    cred_total			INT NOT NULL,
+    id_plan   INT AUTO_INCREMENT PRIMARY KEY,
+    desc_plan   VARCHAR(64) NOT NULL,
+    no_plan		   NUMERIC(3), -- 20, 21, etc
+    abr_carr		   CHAR(1) NOT NULL,
+    cred_total	    INT NOT NULL,
     FOREIGN KEY (abr_carr) REFERENCES Carrera(abr_carr)
 );
 
 CREATE TABLE Edificio (
-    id_edificio 		INT AUTO_INCREMENT PRIMARY KEY,
-    desc_edificio 		VARCHAR(64) NOT NULL,
-    abr_edificio		CHAR(3),
-    id_inst				INT NOT NULL,
+    id_edificio      INT AUTO_INCREMENT PRIMARY KEY,
+    desc_edificio     	VARCHAR(64) NOT NULL,
+    abr_edificio    CHAR(3),
+    id_inst		     INT NOT NULL,
     FOREIGN KEY (id_inst) REFERENCES Institucion(id_inst)
 );
 
 CREATE TABLE Academia (
-    id_academia 		INT AUTO_INCREMENT PRIMARY KEY,
-    nom_academia 		VARCHAR(64) NOT NULL,
-    id_edificio			INT NOT NULL,
+    id_academia 	   INT AUTO_INCREMENT PRIMARY KEY,
+    nom_academia      VARCHAR(64) NOT NULL,
+    id_edificio		    INT NOT NULL,
     FOREIGN KEY (id_edificio) REFERENCES Edificio(id_edificio)
 );
 
 CREATE TABLE Materia (
-    id_materia 			INT AUTO_INCREMENT PRIMARY KEY,
-    tipo_materia 		VARCHAR(20) NOT NULL, -- Obligatoria u Optativa
-    nom_materia 		VARCHAR(64) NOT NULL,
-    horas_teoria		INT,
-    horas_prac			INT,
-    id_academia			INT,
+    id_materia 	    INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_materia 	    VARCHAR(20) NOT NULL, -- Obligatoria u Optativa
+    nom_materia      VARCHAR(64) NOT NULL,
+    horas_teoria	    INT,
+    horas_prac		    INT,
+    id_academia		    INT,
     FOREIGN KEY (id_academia) REFERENCES Academia (id_academia)
     -- tiempo_auto		 	INT
 );
 
 -- Abr Carrera, id_mat, semestre
 CREATE TABLE Mapa_Curricular (
-	abr_carr			CHAR(1) NOT NULL,
-    id_plan	 			INT NOT NULL,
-    id_materia 			INT NOT NULL,
-    semestre 			INT NOT NULL,
-    creditos			INT NOT NULL,
-    no_materia			CHAR(2),
+	abr_carr	    CHAR(1) NOT NULL,
+    id_plan	 	    INT NOT NULL,
+    id_materia 	    INT NOT NULL,
+    semestre 	    INT NOT NULL,
+    creditos	    INT NOT NULL,
+    no_materia		    CHAR(2),
     PRIMARY KEY (abr_carr, id_plan, id_materia),
     FOREIGN KEY (id_plan) REFERENCES Plan_Estudios(id_plan),
     FOREIGN KEY (id_materia) REFERENCES Materia(id_materia),
@@ -68,41 +68,41 @@ CREATE TABLE Mapa_Curricular (
 );
 
 CREATE TABLE Alumno (
-	no_boleta			NUMERIC(10),
-    id_plan	 			INT NOT NULL,
-    nom_al 				VARCHAR(64) NOT NULL,
-    ap_al 				VARCHAR(64) NOT NULL,
-    am_al 				VARCHAR(64) NOT NULL,
-    curp 				VARCHAR(20),
-    email_p_alumno 		VARCHAR(128) NOT NULL,
-    email_i_alumno 		VARCHAR(128),
-    telf_alumno 		VARCHAR(12),
-    telm_alumno 		VARCHAR(12),
-    calle				VARCHAR(64),
-    no_ext				VARCHAR(10),
-    no_int				VARCHAR(10),
-    colonia				VARCHAR(64),
-    delegacion			VARCHAR(64),
-    cp					NUMERIC(5),
+	no_boleta		    NUMERIC(10),
+    id_plan	 		    INT NOT NULL,
+    nom_al 			    VARCHAR(64) NOT NULL,
+    ap_al 			    VARCHAR(64) NOT NULL,
+    am_al 			    VARCHAR(64) NOT NULL,
+    curp 			    VARCHAR(20),
+    email_p_alumno 	   VARCHAR(128) NOT NULL,
+    email_i_alumno 	    VARCHAR(128),
+    telf_alumno 	    VARCHAR(12),
+    telm_alumno 	    VARCHAR(12),
+    calle			   VARCHAR(64),
+    no_ext			   VARCHAR(10),
+    no_int			    VARCHAR(10),
+    colonia			    VARCHAR(64),
+    delegacion		   VARCHAR(64),
+    cp				   NUMERIC(5),
     CONSTRAINT no_boleta PRIMARY KEY (no_boleta),
     FOREIGN KEY (id_plan) REFERENCES Plan_Estudios(id_plan)
 );
 
 CREATE TABLE Docente (
-    rfc 				VARCHAR(13) PRIMARY KEY,
-    id_academia 		INT NOT NULL,
-    nom_doc 			VARCHAR(64) NOT NULL,
-    ap_doc 				VARCHAR(64) NOT NULL,
-    am_doc	 			VARCHAR(64) NOT NULL,
-    email_p_doc	 		VARCHAR(128),
-    email_i_doc 		VARCHAR(128),
-    tel_doc 			VARCHAR(10),
-    calle				VARCHAR(64),
-    no_ext				VARCHAR(10),
-    no_int				VARCHAR(10),
-    colonia				VARCHAR(64),
-    delegacion			VARCHAR(64),
-    cp					NUMERIC(5),
+    rfc 			    VARCHAR(13) PRIMARY KEY,
+    id_academia 	    INT NOT NULL,
+    nom_doc 		    VARCHAR(64) NOT NULL,
+    ap_doc 			    VARCHAR(64) NOT NULL,
+    am_doc	 		    VARCHAR(64) NOT NULL,
+    email_p_doc	 	    VARCHAR(128),
+    email_i_doc 	    VARCHAR(128),
+    tel_doc 		    VARCHAR(10),
+    calle			    VARCHAR(64),
+    no_ext			    VARCHAR(10),
+    no_int			    VARCHAR(10),
+    colonia			    VARCHAR(64),
+    delegacion		    VARCHAR(64),
+    cp				    NUMERIC(5),
     FOREIGN KEY (id_academia) REFERENCES Academia(id_academia)
 );
 
@@ -112,11 +112,11 @@ CREATE TABLE Periodo_Escolar (
 );
 
 CREATE TABLE Grupo(
-	semestre 			INT NOT NULL,
-    abr_carr			CHAR(1),
-    turno 				CHAR(1),
-    no_grupo 			INT NOT NULL,
-    id_periodo 			INT NOT NULL,
+	semestre 		    INT NOT NULL,
+    abr_carr		    CHAR(1),
+    turno 			    CHAR(1),
+    no_grupo 		    INT NOT NULL,
+    id_periodo 		    INT NOT NULL,
     PRIMARY KEY (semestre, abr_carr, turno, no_grupo, id_periodo),
     CHECK (turno IN('M', 'V')),
     FOREIGN KEY (abr_carr) REFERENCES Carrera(abr_carr),
@@ -124,30 +124,30 @@ CREATE TABLE Grupo(
 );
 
 CREATE TABLE Grupo_Horario (
-	semestre 			INT NOT NULL,
-    abr_carr			CHAR(1),
-    turno 				CHAR(1),
-    no_grupo 			INT NOT NULL,
-    id_periodo 			INT NOT NULL,
-    id_materia 			INT NOT NULL,
-    cupo				INT,
-    disponibles			INT DEFAULT 40,
-    rfc					VARCHAR(13),
-	lun_i 				TIME,
-    lun_f				TIME,
-    lun_sal				VARCHAR(10),
-    mar_i 				TIME,
-    mar_f				TIME,
-    mar_sal				VARCHAR(10),
-    mie_i 				TIME,
-    mie_f				TIME,
-    mie_sal				VARCHAR(10),
-    jue_i 				TIME,
-    jue_f				TIME,
-    jue_sal				VARCHAR(10),
-    vie_i 				TIME,
-    vie_f				TIME,
-    vie_sal				VARCHAR(10),
+	semestre 		    INT NOT NULL,
+    abr_carr		    CHAR(1),
+    turno 			    CHAR(1),
+    no_grupo 		    INT NOT NULL,
+    id_periodo 		    INT NOT NULL,
+    id_materia 		    INT NOT NULL,
+    cupo			    INT,
+    disponibles		    INT DEFAULT 40,
+    rfc				    VARCHAR(13),
+	lun_i 			    TIME,
+    lun_f			    TIME,
+    lun_sal			    VARCHAR(10),
+    mar_i 			    TIME,
+    mar_f			    TIME,
+    mar_sal			    VARCHAR(10),
+    mie_i 			    TIME,
+    mie_f			    TIME,
+    mie_sal			    VARCHAR(10),
+    jue_i 			    TIME,
+    jue_f			    TIME,
+    jue_sal			    VARCHAR(10),
+    vie_i 			    TIME,
+    vie_f			    TIME,
+    vie_sal			    VARCHAR(10),
     PRIMARY KEY (semestre, abr_carr, turno, no_grupo, id_periodo, id_materia, rfc),
     FOREIGN KEY (semestre, abr_carr, turno, no_grupo, id_periodo) 
         REFERENCES Grupo (semestre, abr_carr, turno, no_grupo, id_periodo),
@@ -156,28 +156,28 @@ CREATE TABLE Grupo_Horario (
 );
 
 CREATE TABLE Inscripcion (
-    no_boleta 			NUMERIC(10) NOT NULL,
-    id_periodo 			INT NOT NULL,
-    fecha_inscripcion 	DATE NOT NULL,
+    no_boleta 		    NUMERIC(10) NOT NULL,
+    id_periodo 		    INT NOT NULL,
+    fecha_inscripcion     DATE NOT NULL,
     PRIMARY KEY (no_boleta, id_periodo),
     FOREIGN KEY (no_boleta) REFERENCES Alumno(no_boleta),
     FOREIGN KEY (id_periodo) REFERENCES Periodo_Escolar(id_periodo)
 );
 
 CREATE TABLE Inscripcion_Detalle (
-    no_boleta			NUMERIC(10),
-    semestre 			INT NOT NULL,
-    abr_carr			CHAR(1),
-    turno 				CHAR(1),
-    no_grupo 			INT NOT NULL,
-    id_periodo 			INT NOT NULL,
-    id_materia 			INT NOT NULL,
-    rfc		 			VARCHAR(13),
-    cal_parcial_1 		INT,
-    cal_parcial_2 		INT,
-    cal_parcial_3 		INT,
-	cal_extra			INT, 
-    cal_final 			INT,
+    no_boleta		    NUMERIC(10),
+    semestre 		    INT NOT NULL,
+    abr_carr		    CHAR(1),
+    turno 			    CHAR(1),
+    no_grupo 		    INT NOT NULL,
+    id_periodo 		    INT NOT NULL,
+    id_materia 		    INT NOT NULL,
+    rfc		 		    VARCHAR(13),
+    cal_parcial_1 	    INT,
+    cal_parcial_2 	    INT,
+    cal_parcial_3 	    INT,
+	cal_extra		    INT, 
+    cal_final 		    INT,
     FOREIGN KEY (no_boleta, id_periodo)
 		REFERENCES Inscripcion (no_boleta, id_periodo),
     FOREIGN KEY (semestre, abr_carr, turno, no_grupo, id_periodo, id_materia, rfc)
@@ -185,20 +185,20 @@ CREATE TABLE Inscripcion_Detalle (
 );
 
 CREATE TABLE Historial_Academico (
-	id_historial		INT PRIMARY KEY AUTO_INCREMENT,
-    no_boleta			NUMERIC (10) NOT NULL,
-    promedio			FLOAT DEFAULT 0.0,
+	id_historial	    INT PRIMARY KEY AUTO_INCREMENT,
+    no_boleta		    NUMERIC (10) NOT NULL,
+    promedio		    FLOAT DEFAULT 0.0,
     FOREIGN KEY (no_boleta) REFERENCES Alumno (no_boleta)
 );
 
 CREATE TABLE Historial_Detalle (
-	id_historial		INT NOT NULL,
-    id_plan				INT NOT NULL,
-    semestre			INT NOT NULL,
-    id_materia			INT NOT NULL,
-    calificacion		INT NOT NULL,
-    id_periodo			INT NOT NULL,
-    forma_eval			VARCHAR(3) CHECK(forma_eval IN('ORD', 'REC', 'EXT', 'ETS', 'EQV')),
+	id_historial	    INT NOT NULL,
+    id_plan			    INT NOT NULL,
+    semestre		    INT NOT NULL,
+    id_materia		    INT NOT NULL,
+    calificacion	    INT NOT NULL,
+    id_periodo		    INT NOT NULL,
+    forma_eval		    VARCHAR(3) CHECK(forma_eval IN('ORD', 'REC', 'EXT', 'ETS', 'EQV')),
     FOREIGN KEY (id_historial) REFERENCES Historial_Academico(id_historial),
     FOREIGN KEY (id_plan) REFERENCES Plan_Estudios(id_plan),
     FOREIGN KEY (id_periodo) REFERENCES Periodo_Escolar(id_periodo),
@@ -206,20 +206,20 @@ CREATE TABLE Historial_Detalle (
 );
 
 CREATE TABLE Estado_General (
-	no_boleta			NUMERIC (10) NOT NULL,
-    id_materia			INT NOT NULL,
-    estado				VARCHAR(10) CHECK (estado IN ('REPROBADA', 'NO CURSADA', 'DESFASADA')),
+	no_boleta		    NUMERIC (10) NOT NULL,
+    id_materia		    INT NOT NULL,
+    estado			    VARCHAR(10) CHECK (estado IN ('REPROBADA', 'NO CURSADA', 'DESFASADA')),
     FOREIGN KEY (no_boleta) REFERENCES Alumno (no_boleta),
     FOREIGN KEY (id_materia) REFERENCES Materia (id_materia)
 );
 
 CREATE TABLE Trayectoria_Alumno (
-	no_boleta			NUMERIC(10),
-    per_cur				INT DEFAULT 0,
-    per_dis				INT,
-    cred_per			FLOAT,
-    cred_fal			FLOAT,
-    cred_obt			FLOAT,
+	no_boleta		    NUMERIC(10),
+    per_cur			    INT DEFAULT 0,
+    per_dis			    INT,
+    cred_per		    FLOAT,
+    cred_fal		    FLOAT,
+    cred_obt		    FLOAT,
     FOREIGN KEY (no_boleta) REFERENCES Alumno (no_boleta)
 );
 
--- project/_view/template/master.php
+++
@@ -1,5 +0,0 @@
-<?php
-include "_view/template/head.php";
-include '_view/template/navbar_'. $_SESSION['tipo'] .'.php'; 
-$ctrl->renderContent();
-include "_view/template/footer.php";
