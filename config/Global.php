2a1f33ce9592b203ad34d697c96fa4506ffbd4c3
Gus524 <e.gus.gg@gmail.com>
2025-06-28 20:55:34 -0600

    Fix::17::Testing roots

--- config/Global.php
+++ config/Global.php
@@ -2,5 +2,4 @@
 mb_internal_encoding('UTF-8');
 
 define("SITE_URL", "https://schoolshield.azurewebsites.net");
-define("ROOT_PATH", realpath(__DIR__ . '/..') . '/');
 define("DEFAULT_URL", "Inicio");
