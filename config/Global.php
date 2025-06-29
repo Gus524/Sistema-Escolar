93432ea863acee1dc9115ace840a873a234cee00
Gus524 <e.gus.gg@gmail.com>
2025-06-28 22:15:33 -0600

    Fix::23::Correccion en utilidades

--- config/Global.php
+++ config/Global.php
@@ -1,5 +1,12 @@
 <?php
-mb_internal_encoding('UTF-8');
+set_include_path(
+    get_include_path() .
+    PATH_SEPARATOR . realpath(__DIR__ . '/..') . '/_model' .
+    PATH_SEPARATOR . realpath(__DIR__ . '/..') . '/_controller' .
+    PATH_SEPARATOR . realpath(__DIR__ . '/..') . '/_view' .
+    PATH_SEPARATOR . realpath(__DIR__ . '/..') . '/ajax' .
+    PATH_SEPARATOR . realpath(__DIR__ . '/..') . '/DAO'
+);
 
 define("SITE_URL", "https://schoolshield.azurewebsites.net");
 define("DEFAULT_URL", "Inicio");
