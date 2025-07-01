7b2fc34962392123ae6ee56434b6d1259d895a3a
Gus524 <e.gus.gg@gmail.com>
2025-06-30 21:49:19 -0600

    Fix::Demo-ip::3::Corrigiendo funcion para obtener ip real para auditoria

--- utilidades/utilidades.php
+++ utilidades/utilidades.php
@@ -166,7 +166,7 @@ class Utilidades
             exit();
         }
         // Si el usuario no tiene permiso para acceder a la pagina manda error
-        if(!self::has_permissions($tipo, $page)){
+        if(!self::has_permissions($tipo, $page) && $page != 'login' && $page != 'Cerrar' && $page != 'js') {
             $details = "Intento de acceso a la pagina " . $page . " por el usuario {$_SESSION['user']} con tipo $tipo";
             self::save_log_audit('Acceso denegado', $_SESSION['user'], $details);
             self::show_error(403, "Acceso denegado");
